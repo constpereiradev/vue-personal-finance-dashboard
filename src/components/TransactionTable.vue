c8f78241d6abda16e5f931ab61d355367eec4097
constpereiradev <amandapereiradevcontact@gmail.com>
2026-03-05 17:23:37 -0300

    feat: Ajustes UI dashboard

--- src/components/TransactionTable.vue
+++ src/components/TransactionTable.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-
 const props = defineProps({
     data: {
         required: true,
@@ -8,43 +6,57 @@ const props = defineProps({
     }
 })
 
+type Transaction = Object
+
 const emit = defineEmits(['delete-transaction'])
 
-const deleteTransaction = (transaction) => {
+const deleteTransaction = (transaction: Transaction) => {
     emit('delete-transaction', transaction)
 }
-
 </script>
 
 <template>
-    <div v-if="props.data.transactions.length > 0" class="row bg-secondary-subtle p-5">
-        <div class="col-sm-12 ">
-            <div class="row">
-                <div class="col-sm-12">
-                    <h3>Transações</h3>
-                </div>
-            </div>
+    <div v-if="props.data.transactions.length > 0" class="">
 
-            <table class="table table-secondary">
-                <thead>
-                    <tr>
-                        <th scope="col"><strong>Título</strong></th>
-                        <th scope="col"><strong>Categoria</strong></th>
-                        <th scope="col"><strong>Valor</strong></th>
-                        <th scope="col"><strong>Ação</strong></th>
+        <div class="overflow-hidden rounded-xl shadow border border-gray-200">
+            <table class="min-w-full bg-white">
+
+                <thead class="bg-gray-500">
+                    <tr class="text-left text-sm text-black tracking-wider">
+                        <th class="px-6 py-3 fs-5">Título</th>
+                        <th class="px-6 py-3 fs-5">Categoria</th>
+                        <th class="px-6 py-3 fs-5">Valor</th>
+                        <th class="px-6 py-3 fs-5 text-center">Ação</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr v-for="transaction in props.data.transactions || []" :key="transaction.id">
-                        <td>{{ transaction.title }}</td>
-                        <td>{{ transaction.category }}</td>
-                        <td>{{ transaction.value }}</td>
-                        <td>
-                            <button v-on:click="deleteTransaction(transaction)" class="btn btn-danger">Deletar</button>
+
+                <tbody class="divide-y divide-gray-200">
+
+                    <tr v-for="transaction in props.data.transactions || []" :key="transaction.id"
+                        class="hover:bg-gray-50 transition">
+                        <td class="px-6 py-4 text-gray-800">
+                            {{ transaction.title }}
+                        </td>
+
+                        <td class="px-6 py-4 text-gray-600">
+                            {{ transaction.category }}
+                        </td>
+
+                        <td class="px-6 py-4 font-semibold text-gray-900">
+                            R$ {{ transaction.value }}
+                        </td>
+
+                        <td class="px-6 py-4 text-center">
+                            <button @click="deleteTransaction(transaction)"
+                                class="bg-red-500 hover:bg-red-600 text-white text-sm px-4 py-2 rounded-lg transition">
+                                Deletar
+                            </button>
                         </td>
                     </tr>
+
                 </tbody>
             </table>
         </div>
+
     </div>
 </template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,26 +1,23 @@
 <script setup lang="ts">
 
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import axios from 'axios';
 
 import TransactionTable from '@/components/TransactionTable.vue';
 import NewTransactionModel from '@/components/NewTransactionModel.vue';
-
-const expensesTitle = ref('Gastos - R$: ')
-const incomesTitle = ref('Redimentos - R$: ')
-const noTransactionMessage = ref('Nenhuma transação para visualizar.')
+import GranoHeader from '@/components/GranoHeader.vue';
 const from = ref('')
 const to = ref('')
 const showNewTransactionModel = ref(false)
+const balance = ref(0)
 
-const title = ref('')
-const category = ref('')
-const type = ref('')
-const value = ref(0)
+const title = ref<string>('')
+const category = ref<string>('')
+const type = ref<string>('')
+const value = ref<number>(0)
+const viewExpenses = ref<boolean>(false)
+const viewIncomes = ref<boolean>(false)
 
-const transactionId = ref(null)
-
-const token = localStorage.getItem('token')
 const incomes = ref(
     {
         total: 0,
@@ -33,7 +30,33 @@ const expenses = ref(
         transactions: []
     }
 )
-const balance = ref(0)
+
+
+const financeDatas = computed(() => [
+    {
+        title: 'Saldo',
+        value: 'R$' + balance.value
+    },
+    {
+        title: 'Gastos',
+        value: 'R$' + expenses.value.total
+    },
+    {
+        title: 'Rendimentos',
+        value: 'R$' + incomes.value.total
+    },
+])
+
+const transactionId = ref<number | null>(null)
+
+interface Transaction {
+    id: number
+    title: string
+    category: string
+    type: string
+    value: number
+}
+
 
 const loading = ref(false)
 
@@ -46,9 +69,6 @@ const getSummaryData = () => {
         params: {
             from: from.value,
             to: to.value
-        },
-        headers: {
-            'Authorization': 'Bearer ' + token
         }
     })
         .then((response) => {
@@ -70,11 +90,6 @@ const registerTransaction = () => {
             category: category.value,
             type: type.value,
             value: value.value,
-        },
-        {
-            headers: {
-                'Authorization': 'Bearer ' + token
-            }
         }
     ).then((response) => {
         getSummaryData()
@@ -86,9 +101,6 @@ const registerTransaction = () => {
 
 const deleteTransaction = () => {
     axios.delete('api/finance-transaction/' + transactionId.value, {
-        headers: {
-            'Authorization': 'Bearer ' + token
-        }
     }).then((response => {
         getSummaryData()
         console.log(response)
@@ -102,7 +114,7 @@ const closeModalHandler = () => {
     showNewTransactionModel.value = false
 }
 
-const handleRegisterTransaction = (data) => {
+const handleRegisterTransaction = (data: Transaction) => {
     title.value = data.title
     category.value = data.category
     type.value = data.type
@@ -111,7 +123,7 @@ const handleRegisterTransaction = (data) => {
     registerTransaction()
 }
 
-const handleDeleteTransaction = (transaction) => {
+const handleDeleteTransaction = (transaction: Transaction) => {
     transactionId.value = transaction.id
     deleteTransaction()
 }
@@ -119,58 +131,112 @@ const handleDeleteTransaction = (transaction) => {
 
 <template>
 
-    <NewTransactionModel v-on:close-modal="closeModalHandler" v-on:register-transaction="handleRegisterTransaction"
-        v-if="showNewTransactionModel" />
+    <div class="bg-gray-900 min-h-screen text-white w-full">
 
-    Saldo: {{ balance }}
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="row">
-                <div class="col-sm-6">
-                    De: <input type="date" v-model="from">
-                    Até: <input type="date" v-model="to">
+        <GranoHeader />
+
+        <div class="max-w-6xl mx-auto px-6 py-10 space-y-12">
+            <div class="flex items-end justify-between">
+
+                <div class="flex gap-4">
+                    <div class="flex flex-col">
+                        <label for="from" class="text-sm font-medium text-white mb-1">
+                            De
+                        </label>
+                        <input v-model="from" id="from" type="date" name="from"
+                            class="bg-white/5 border border-white/10 rounded-md px-3 py-2 text-white focus:outline-none focus:border-indigo-500" />
+                    </div>
+
+                    <div class="flex flex-col">
+                        <label for="to" class="text-sm font-medium text-white mb-1">
+                            Até
+                        </label>
+                        <input v-model="to" id="to" type="date" name="to"
+                            class="bg-white/5 border border-white/10 rounded-md px-3 py-2 text-white focus:outline-none focus:border-indigo-500" />
+                    </div>
                 </div>
 
-                <div class="col-sm-3">
-                    <button v-on:click="getSummaryData" class="btn btn-primary">Buscar</button>
+                <button v-on:click="getSummaryData"
+                    class="bg-indigo-600 hover:bg-indigo-500 text-white px-5 py-2 rounded-md font-medium transition">
+                    Buscar
+                </button>
+
+            </div>
+        </div>
+        <div class="max-w-6xl mx-auto px-6 py-10 space-y-12">
+
+            <!-- CARDS -->
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6 pb-10 ">
+
+                <div v-for="financeData in financeDatas" :key="financeData.title"
+                    class="bg-gray-800 p-6 rounded-xl border border-gray-700 hover:border-amber-500 transition">
+                    <p class="text-gray-400 text-sm">
+                        {{ financeData.title }}
+                    </p>
+
+                    <p class="text-2xl font-bold mt-2" :class="{
+                        'text-green-400': financeData.title === 'Rendimentos',
+                        'text-red-400': financeData.title === 'Gastos',
+                        'text-white': financeData.title === 'Saldo'
+                    }">
+                        {{ financeData.value }}
+                    </p>
                 </div>
 
-                <div class="col-sm-3">
-                    <button v-on:click="showNewTransactionModel = true" class="btn btn-primary">Cadastrar
-                        transação</button>
+            </div>
+
+            <!-- TABELA GASTOS -->
+            <div class="bg-gray-800 rounded-xl border border-gray-700 shadow-lg overflow-hidden">
+
+                <div @click="viewExpenses = !viewExpenses"
+                    class="flex items-center justify-between px-6 py-4 border-b border-gray-700">
+                    <h3 class="text-lg font-semibold">
+                        Gastos
+                    </h3>
+
+                    <span class="text-sm text-gray-400">
+                        {{ expenses.transactions.length }} registros
+                    </span>
+                </div>
+
+                <div v-if="viewExpenses" class="p-6 ">
+                    <TransactionTable v-if="expenses.transactions.length" :data="expenses"
+                        @delete-transaction="handleDeleteTransaction" />
+
+                    <div v-else class="text-center text-gray-500 py-10">
+                        Nenhum gasto registrado
+                    </div>
+
                 </div>
             </div>
+
+            <br>
+            <div class="bg-gray-800 rounded-xl border border-gray-700 shadow-lg overflow-hidden">
+
+                <div @click="viewIncomes = !viewIncomes"
+                    class="flex items-center justify-between px-6 py-4 border-b border-gray-700">
+                    <h3 class="text-lg font-semibold">
+                        Rendimentos
+                    </h3>
+
+                    <span class="text-sm text-gray-400">
+                        {{ incomes.transactions.length }} registros
+                    </span>
+                </div>
+
+                <div v-if="viewIncomes" class="p-6 ">
+                    <TransactionTable v-if="incomes.transactions.length" :data="incomes"
+                        @delete-transaction="handleDeleteTransaction" />
+
+                    <div v-else class="text-center text-gray-500 py-10">
+                        Nenhum gasto registrado
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     </div>
 
-    <div class="row mb-3">
-        <div class="col-sm-12">
-            <h1>{{ expensesTitle }}{{ expenses.total }}</h1>
-        </div>
-        <div class="col-sm-12">
-            <div v-if="!expenses.transactions.length" class="row">
-                <div class="col-sm-12">
-                    {{ noTransactionMessage }}
-                </div>
-            </div>
-
-            <TransactionTable v-on:delete-transaction="handleDeleteTransaction" :data="expenses"></TransactionTable>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-sm-12">
-            <h1>{{ incomesTitle }}{{ incomes.total }}</h1>
-        </div>
-        <div class="col-sm-12">
-            <div v-if="!incomes.transactions.length" class="row">
-                <div class="col-sm-12">
-                    {{ noTransactionMessage }}
-                </div>
-            </div>
-
-            <TransactionTable v-on:delete-transaction="handleDeleteTransaction" :data="incomes"></TransactionTable>
-        </div>
-    </div>
     <RouterView />
 </template>
